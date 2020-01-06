e3ec1e1af36010b660e1c37a30c02c43648eff79
Andrei K <1____1@bk.ru>
2020-01-07 00:30:41 +0300

    count avg salaries by lang

--- vacancies.py
+++ vacancies.py
@@ -1,19 +1,56 @@
 import requests
 
-def get_vacancies(lang):
+
+def get_hh_response(lang):
     url = 'https://api.hh.ru/vacancies'
-    payload = {'text':f'Программист {lang}', "areas":"Москва"}
+    payload = {'text':f'Программист {lang}', "areas":"Москва",
+               "only_with_salary": True}
     response = requests.get(url, params=payload)
 
     return response.json()
 
 
-if __name__ == "__main__":
+def predict_rub_salary(salary):
+    if salary.get('currency') != "RUR":
+        return None
+    low = salary.get('from')
+    high = salary.get('to')
+    if low and high:
+        return (low + high) / 2
+    if low:
+        return low * 1.2
+    return high * 0.8
+
+
+def get_average_salary(salaries):
+    salary_sum = 0
+    value = 0
+    for salary in salaries:
+        if salary:
+            salary_sum += salary
+            value += 1
+    return int(salary_sum/value)
+
+
+def main():
     languages = ["Python", "Java", "Javascript", "Ruby",
                  "PHP", "C++", "C", "Go"]
     vac = {}
     for language in languages:
-        resp = get_vacancies(language)
-        value = resp.get('found')
-        vac[language] = value
+        response = get_hh_response(language)
+        value = response.get('found')
+        vacancies = response.get('items')
+        salaries = []
+        for vacancy in vacancies:
+            salary = vacancy.get('salary')
+            salaries.append(predict_rub_salary(salary))
+        avg_salary = get_average_salary(salaries)
+        vac[language] = {"vacancies_found": value,
+                         "vacancies_processed":len(vacancies),
+                         "average_salary":avg_salary
+                         }
     print(vac)
+
+
+if __name__ == "__main__":
+    main()
